5bf0fda5f6aa277a2908ad5c5b1f9749319cd7f5
Edward Boland <mr.eboland@gmail.com>
2021-01-21 17:45:33 -0500

    practice questions

--- greeter.py
+++ greeter.py
@@ -128,3 +128,37 @@ def describePet(animalType, petName):
 # By calling the function that has parameters without arguments will result in a type error about required positional arguments. This also applies if you give to many arguments for the amount of parameters in the function
 # Python helps us out by listing the arguments that weren't passed to the function. This is a good reason to have good naming schemes for our arguments so we know what it's for.
 # describePet()
+
+
+# 8-3. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function once using positional arguments to make a shirt. Call the function a second time using keyword arguments.
+
+def makeShirt(size, text):
+    print(f"\nYou've select a size {size}, with '{text}' written on it")
+    
+makeShirt("large", "Python is the best!")
+makeShirt(size="large", text="Python is the best!")
+
+# 8-4. Large Shirts: Modify the make_shirt() function so that shirts are large
+# by default with a message that reads I love Python. Make a large shirt and a
+# medium shirt with the default message, and a shirt of any size with a different
+# message.
+
+
+def makeShirt(text, size="large"):
+    print(f"\nYou've select a size {size}, with '{text}' written on it")
+    
+makeShirt("I love Python")
+makeShirt("I love Python", "medium")
+makeShirt("Python rules!", "small")
+
+# 8-5. Cities: Write a function called describe_city() that accepts the name of
+# a city and its country. The function should print a simple sentence, such as
+# Reykjavik is in Iceland. Give the parameter for the country a default value.
+# Call your function for three different cities, at least one of which is not in the default country.
+def describeCity(name, country="canada"):
+    print(f"\n{name.title()} is in {country.title()}.")
+    
+describeCity("toronto")
+describeCity("ottawa")
+describeCity("tokyo", "japan")
+describeCity("saigon", "vietnam")
